e14e488c57233954c2ca0ea1899ad6629f485c7a
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-07 03:00:38 -0700

    Update model.h

--- model.h
+++ model.h
@@ -1,1 +1,7 @@
 
+
+typedef struct             /* type definition for bullet object */
+{
+unsigned int x, y;         /* position coordinates */
+int delta_x, delta_y;      /* horiz. & vert. displacement per clock tick */
+} Bullet;
