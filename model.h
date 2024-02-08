157f865c807bcef7223e0ddc768232a5e564ce1d
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-07 19:11:03 -0700

    Update model.h

--- model.h
+++ model.h
@@ -1,7 +1,49 @@
 
+typedef struct 
+{
+unsigned int x;
+unsigned int y;
+} Position;
+
+typedef struct 
+{
+int height;
+int width;
+} Size;
+
+typedef struct 
+{
+Position position;
+Size size; 
+} BoundingBox;
 
 typedef struct             /* type definition for bullet object */
 {
-unsigned int x, y;         /* position coordinates */
+Position position;         /* position coordinates */
 int delta_x, delta_y;      /* horiz. & vert. displacement per clock tick */
 } Bullet;
+
+typedef struct             /* type definition for cowboy object */
+{
+Position position;        /* position coordinates */
+Size size;
+int horizontalDirection;   /* direction the cowboy is facing*/
+int verticalDirection;
+int horizontalVelocity;    /* horiz. & vert. displacement per clock tick */
+int verticalVelocity;
+bool isMoving;
+bool isFiring;
+int verticalFiringDirection;
+int horizontalFiringDirection;
+} Cowboy;
+
+typedef struct 
+{
+Position position;
+Size size;
+int horizontalDirection;
+int verticalDirection;
+int horizontalVelocity;
+int verticalVelocity;
+Position target;
+} Snake;
