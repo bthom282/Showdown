e0d833c25d897ce211927a1a832e27f991dd891e
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-11 16:53:01 -0700

    Update model.c

--- model.c
+++ model.c
@@ -25,3 +25,14 @@ void game_quit()
 {
 
 }
+
+void print_message(UINT8 *base, char message[], int x_pos, int y_pos) {
+	int i = 0;
+	if (x_pos >= 0 && x_pos < SCREEN_WIDTH && y_pos >= 0 && y_pos < SCREEN_HEIGHT) {
+		while (message[i] != '\0') {
+			plot_char((UINT8 *) base, x_pos, y_pos, message[i]);
+			x_pos += 8;
+			i++;
+		}
+	}
+}
