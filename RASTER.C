3d2001d17f64a1a729c1c0caf9cf2d9bffc4aa64
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-11 23:35:57 -0700

    Update RASTER.C

--- RASTER.C
+++ RASTER.C
@@ -118,6 +118,52 @@ void plot_bitmap_32(UINT32 *base, int x, int y, const UINT32 *bitmap32, unsigned
 }
 
 /********************************************************************************************
+Function Name: plot_bitmap_8
+Details: This function is used to plot a size 8 bitmap to the screen by ORing. 
+This will be used for the bullets in the game as they appear and travel across
+the screen.
+
+*********************************************************************************************/
+
+void plot_bitmap_8(UINT8 *base, int x, int y, const UINT8 *bitmap8, unsigned int height)
+{
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
+		int i;
+		base += (x/8) + y * 80;
+		for(i = 0; i < height; i++)
+		{
+			*base |= bitmap8[i] >> x % 8;
+			*(base + 1) |= bitmap8[i] << 8 - (x % 8);
+			base += 80;
+		}
+	}
+	return;
+}
+
+/********************************************************************************************
+Function Name: clear_bitmap_8
+Details: This function is used to clear a size 8 bitmap from the screen by XORing over the 
+previously plotted bitmap. This will be used for the bullets in the game as they travel across
+the screen.
+
+*********************************************************************************************/
+
+void clear_bitmap_8(UINT8 *base, int x, int y, const UINT8 *bitmap8, unsigned int height)
+{
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT) {
+		int i;
+		base += (x/8) + y * 80;
+		for(i = 0; i < height; i++)
+		{
+			*base ^= bitmap8[i] >> x % 8;
+			*(base + 1) ^= bitmap8[i] << 8 - (x % 8);
+			base += 80;
+		}
+	}
+	return;
+}
+
+/********************************************************************************************
 Function Name: plot_char
 Details: 
 
