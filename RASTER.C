041f41066fe8f2e9c5be3034aee95b4db13b00a7
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-13 11:58:39 -0700

    Update RASTER.C

--- RASTER.C
+++ RASTER.C
@@ -10,7 +10,7 @@ void plot_pixel(char *base, int x, int y)
 {
 	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
 		*(base + y * 80 + (x >> 3)) |= 1 << 7 - (x & 7);
-		}
+}
 		
 /********************************************************************************************
 Function Name: plot_vline
