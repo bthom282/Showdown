8d4c2eafca77e17fd3f3dbbbd5d1a7516329cfc0
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-13 22:19:44 -0700

    Update events.c

--- events.c
+++ events.c
@@ -1,5 +1,5 @@
 /*******************************************************************************************
-Names:   Adam Cole, Brooklyn Thomson
+Names:    Adam Cole, Brooklyn Thomson
 
 Purpose:  Implements event-handling functions for each of the specified asynchronous,
           synchronous and condition-based events.
