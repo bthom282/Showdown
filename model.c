b4a2326832f10a67f73dbea7395c006cc398302d
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-13 19:33:28 -0700

    Update model.c

--- model.c
+++ model.c
@@ -6,11 +6,29 @@ Purpose:
 *******************************************************************************************/
 #include "model.h"
 
+/*******************************************************************************************
+Function Name: 	move_bullet
+
+Details: 		
+
+Sample Call:
+
+*********************************************************************************************/
+
 void move_bullet(Bullet *bullet)
 {
   bullet->x += bullet->delta_x;
   bullet->y += bullet->delta_y;
 }
+
+/*******************************************************************************************
+Function Name: 	move_snake
+
+Details: 	This function 	
+
+Sample Call:
+
+*********************************************************************************************/
 
 /*Need to check with Marc if we need to pass in bitmaps and bounding boxes for the move_snake function*/
 
@@ -56,10 +74,31 @@ void move_snake(Snake *snake)
 		
 }
 
+/*******************************************************************************************
+Function Name: 	move_cowboy
+
+Details: 	This function 	
+
+Sample Call:
+
+*********************************************************************************************/
+
 void move_cowboy(Cowboy *cowboy)
 {
 
 }
+
+/*******************************************************************************************
+Function Name: 	cowboy_death
+
+Details: 	This function updates the lives count, called the update lives function, then
+		checks if there are lives remaining. If none, trigger game over sequence by 
+  		calling the game_over function. If lives remaining, triggers the respawn
+    		function.
+
+Sample Call:
+
+*********************************************************************************************/
 
 void cowboy_death (int *lives_count) {
 	/* possible death animation here */
@@ -73,24 +112,60 @@ void cowboy_death (int *lives_count) {
 		update_lives(lives);
 		respawn();
 	}
+
+/*******************************************************************************************
+Function Name: 	update_lives
+
+Details: 	This function is called when the player dies to update the lives shown on the 
+		side panel display.
+
+Sample Call:	lives_count--;
+		update_lives ((UINT16 *) base, p1_lives_count, 320, cowboy_bitmap_16);
+
+*********************************************************************************************/
 	
-void update_lives (UINT8 *base, int lives_count) {
+void update_lives (UINT8 *base, int lives_count, int y, const UINT16 *bitmap16) {
 	int i, x;
 	x = 80;
 	for (i = 0; i < (lives_count+1); i++) {
-		clear_bitmap_16((UINT16 *) base, x, 320, cowboy_bitmap_16, BITMAP_HEIGHT);
+		clear_bitmap_16((UINT16 *) base, x, y, bitmap16, BITMAP_HEIGHT);
 		x += 16;
 	}
 	x = 80;
 	for (i = 0; i < lives_count; i++) {
-		plot_bitmap_16((UINT16 *) base, x, 320, cowboy_bitmap_16, BITMAP_HEIGHT);
+		plot_bitmap_16((UINT16 *) base, x, y, bitmap16, BITMAP_HEIGHT);
 		x += 16;
 	}
 }
 
-void respawn() {
-	plot_bitmap_32((UINT32 *) base, 424, 184, cowboy_bitmap_32, BITMAP_32_HEIGHT);
+/*******************************************************************************************
+Function Name: 	respawn
+
+Details: 	This function is passes in the player instance so it can resets the cowboy's 
+		position to the game start location (x = 424, y = 184), then reprints the 
+  		cowboy bitmap.
+
+Sample Call:	respawn(player1);
+
+*********************************************************************************************/
+
+void respawn(Cowboy *cowboy) {
+	player->x = 424;
+	player->y = 184
+	plot_bitmap_32((UINT32 *) base, 424, 184, cowboy_bitmap_32, BITMAP_32_HEIGHT;
 	/* invulnerability frames code here, asking Marc */
+
+/*******************************************************************************************
+Function Name: 	game_over
+
+Details: 	This function is triggered when the player has run out of lives. It will 
+		print "game over" many times, then indicate the final score somehow, fill
+  		the screen black, then print a message promting the user to either continue
+    		or quit. 	
+
+Sample Call:
+
+*********************************************************************************************/
 
 void game_over((UINT8 *)base, bool isPlaying) {
 	int i,j;
@@ -119,10 +194,28 @@ void game_over((UINT8 *)base, bool isPlaying) {
 	}
 }
 
+/*******************************************************************************************
+Function Name: 	game_quit
+
+Details: 	This function sets the isPlaying bool to false to exit the game loop. 	
+
+Sample Call:
+
+*********************************************************************************************/
+
 void game_quit(bool *isPlaying)
 {
 	isPlaying = FALSE;
 }
+
+/*******************************************************************************************
+Function Name: 	print_message
+
+Details: 	 	
+
+Sample Call:
+
+*********************************************************************************************/
 
 void print_message(UINT8 *base, char message[], int x_pos, int y_pos) {
 	int i = 0;
