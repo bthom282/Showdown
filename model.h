f8caa4bed26b41a8d8dc7232cefd870464900a0b
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-15 11:50:15 -0700

    Update model.h

--- model.h
+++ model.h
@@ -27,46 +27,47 @@ void game_quit(bool *isPlaying);
 
 void print_message(UINT8 *base, char message[], int x_pos, int y_pos);
 
-typedef struct 
+typedef struct Position
 {
 unsigned int x;
 unsigned int y;
-} Position;
+};
 
-typedef struct 
+typedef struct Size
 {
 int height;
 int width;
-} Size;
+};
 
-typedef struct {
-    Size size;
-    Position position;
-} BoundingBox;
+typedef struct BoundingBox
+{
+Size size;
+Position position;
+};
 
-typedef struct            /* type definition for lives object */
+typedef struct Lives          /* type definition for lives object */
 {
 int lives;		  /* default starting lives = 3 */
 Position position;        /* position coordinates (player 1 lives position begins at  (80,320)
                               player 2 lives position begins at  (80,252)) */
-} Lives;
+};
 
-typedef struct            /* type definition for score object */
+typedef struct Score          /* type definition for score object */
 {
 int score;		  /* default initial score is 00000 */
 Position position;        /* position coordinates (player 1 lives position begins at  (80,300)
                               player 2 lives position begins at  (80,232))*/
-} Score;
+};
 
 /*the cactus struct will propbably not get used as we instead set up movement boundary conditions */
 
-typedef struct            /* type definition for cactus object */
+typedef struct Cactus        /* type definition for cactus object */
 {
 Position position;         /* position coordinates */
 BoundingBox boundingBox;
-} Cactus;
+};
 
-typedef struct             /* type definition for bullet object */
+typedef struct Bullet           /* type definition for bullet object */
 {
 Position position;         /* position coordinates */
 int y_dir, x_dir;	   /* horiz. & vert. direction for displacement */
@@ -74,9 +75,9 @@ int y_dir, x_dir;	   /* horiz. & vert. direction for displacement */
       			      y = -1 (traveling up), y = 1 (traveling down) */
 int speed = 3;      	   /* displacement per clock tick for x and y displacement*/
 BoundingBox boundingBox;
-} Bullet;
+};
 
-typedef struct             /* type definition for cowboy object */
+typedef struct Cowboy            /* type definition for cowboy object */
 {
 Position position;        /* position coordinates, 1 player cowboy's initial position is (424, 184) */
 Size size;
@@ -101,9 +102,9 @@ Score score;		   /* default initial score is 00000 */
 Lives lives;		   /* default starting lives = 3 */
 BoundingBox boundingBox;
 const UINT32 bitmap = cowboy_bitmap;
-} Cowboy;
+};
 
-typedef struct             /* type definition for snake object */
+typedef struct Snake           /* type definition for snake object */
 {
 Position position;         /* position coordinates, snakes initially spawn in locations based on a randomizer */
 	                         /* 16 spawn coordinates = {(384,0),(416,0),(448,0),(480,0),
@@ -121,4 +122,4 @@ int state; 		   /* state used for bitmap printing {0 - not moving/ moving down, 
 BoundingBox boundingBox;
 int isDead;
 const UINT32 bitmap = snake_bitmap;
-} Snake;
+};
