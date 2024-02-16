bf812f9fd719270fc78ba171aaeee063e1255a2b
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-16 00:25:26 -0700

    Update events.h

--- events.h
+++ events.h
@@ -1,2 +1,5 @@
 
 int checkCollision(BoundingBox box1, BoundingBox box2);
+
+void shooting(UINT8 *base, struct Cowboy *cowboy, const UINT8 *bitmap8, 
+				struct Bullet *active_bullets, int *bullets_fill);
