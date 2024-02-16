e959433f38062e58590499952c0275d59ef7f770
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-16 01:35:09 -0700

    Update renderer.h

--- renderer.h
+++ renderer.h
@@ -7,3 +7,7 @@ void render_bullet(const *Bullet, UINT8 *base);
 void render_cowboy(const *Cowboy, UINT32 *base);
 
 void render_snake(const *Snake, UINT32 *base);
+
+void render_score{const *Score, UINT8 *base);
+
+void render_lives{const *Lives, UINT16 *base);
