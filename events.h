5e0516d6c2b278bddf859a38a8b3a0ff70734cc2
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-16 11:10:32 -0700

    Update events.h

--- events.h
+++ events.h
@@ -4,4 +4,6 @@ int checkCollision(BoundingBox box1, BoundingBox box2);
 void shooting(UINT8 *base, struct Cowboy *cowboy, const UINT8 *bitmap8, 
 				struct Bullet *active_bullets, int *bullets_fill);
 
+unsigned int randInRange(unsigned long *seed, unsigned int min, unsigned int max);
+
 void spawn_snakes(UINT32 *base, struct Snake *active_snakes, int spawn_x[], int spawn_y[], int *snakes_fill, UINT32 *seed);
