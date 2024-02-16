1f5f27da44b0a78fb09872d4cde950d5ed17562d
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-15 21:10:24 -0700

    Update renderer.h

--- renderer.h
+++ renderer.h
@@ -1,3 +1,9 @@
-
-
 void game_start();
+
+void render(const Model *model, UINT32 *base);
+
+void render_bullet(const *Bullet, UINT8 *base);
+
+void render_cowboy(const *Cowboy, UINT32 *base);
+
+void render_snake(const *Snake, UINT32 *base);
