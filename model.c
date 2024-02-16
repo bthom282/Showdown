5331a7dfdd21fa345d2e90ce3ebaf3bde9ef4224
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-15 20:53:24 -0700

    Update model.c

--- model.c
+++ model.c
@@ -143,18 +143,18 @@ void move_cowboy(UINT32 *base, struct Cowboy *cowboy, const UINT32 *bitmap32)
 {
 	if ((cowboy->isMoving) == TRUE) {
 		Vsync();
-		clear_bitmap_32((UINT32 *) base, cowboy->x_pos, cowboy->y_pos, blank, BITMAP_32_HEIGHT);
-		(cowboy->x_pos) += (cowboy->x_dir)*(cowboy->speed);
-		(cowboy->y_pos) += (cowboy->y_dir)*(cowboy->speed);
-		if ((cowboy->x_pos) > X_MAX)
-			{cowboy->x_pos = X_MAX;}
-		if ((cowboy->x_pos) < X_MIN)
-			{cowboy->x_pos = X_MIN;}
-		if ((cowboy->y_pos) > Y_MAX)
-			{cowboy->y_pos = Y_MAX;}
-		if ((cowboy->y_pos) > X_MIN)
-			{cowboy->y_pos = X_MIN;}
-		plot_bitmap_32((UINT32 *) base, cowboy->x_pos, cowboy->y_pos, cowboy_bitmap_32, BITMAP_32_HEIGHT);
+		clear_bitmap_32((UINT32 *) base, cowboy->xposition->x, cowboy->position->y, blank, BITMAP_32_HEIGHT);
+		(cowboy->position->x) += (cowboy->x_dir)*(cowboy->speed);
+		(cowboy->position->y) += (cowboy->y_dir)*(cowboy->speed);
+		if ((cowboy->position->x) > X_MAX)
+			{cowboy->position->xs = X_MAX;}
+		if ((cowboy->position->x) < X_MIN)
+			{cowboy->position->x = X_MIN;}
+		if ((cowboy->position->y) > Y_MAX)
+			{cowboy->position->y = Y_MAX;}
+		if ((cowboy->position->y) > X_MIN)
+			{cowboy->position->y = X_MIN;}
+		plot_bitmap_32((UINT32 *) base, cowboy->position->x, cowboy->position->y, cowboy_bitmap_32, BITMAP_32_HEIGHT);
 	}
 }
 
