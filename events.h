ac2cac341ac1a7cff01e6b232c7b9d6b0aaa7a3b
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-16 11:16:18 -0700

    Update events.h

--- events.h
+++ events.h
@@ -7,3 +7,5 @@ void shooting(UINT8 *base, struct Cowboy *cowboy, const UINT8 *bitmap8,
 unsigned int randInRange(unsigned long *seed, unsigned int min, unsigned int max);
 
 void spawn_snakes(UINT32 *base, struct Snake *active_snakes, int spawn_x[], int spawn_y[], int *snakes_fill, UINT32 *seed);
+
+void snake_death(struct Snake *snake, int index);
