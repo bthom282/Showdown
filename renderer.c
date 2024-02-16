1170f1fd197ed7d1b5cb6cff0df1cd7574bef663
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-02-15 21:16:40 -0700

    Update renderer.c

--- renderer.c
+++ renderer.c
@@ -47,6 +47,27 @@ while (isPlaying == TRUE) {
 
 }
 
+void render(const Model *model, UINT32 *base)
+{
+
+
+}
+
+void render_bullet(const *Bullet, UINT8 *base)
+{
+
+}
+
+void render_cowboy(const *Cowboy, UINT32 *base)
+{
+
+}
+
+void render_snake(const *Snake, UINT32 *base)
+{
+
+}
+
 void game_start() {
 
   	char *base = Physbase();
