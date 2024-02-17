85c8e3624631c5967ef9d287098c40be78dd00f1
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-17 13:46:00 -0700

    Update RASTER.C

--- RASTER.C
+++ RASTER.C
@@ -206,3 +206,26 @@ void clear_screen(UINT32 *base,int x, int y, int height, int width) {
 	}
 	return;
 }
+
+
+/*******************************************************************************************
+Function Name: 	print_message
+
+Details: 	This function takes in a character array and prints it to the screen starting 
+		at the designated area. It advances automatically to print the full message,
+  		while using the provided font.c file for each character's bitmap.
+
+Sample Call:	print_message((UINT8 *) base, lives, 32, 320);
+
+*********************************************************************************************/
+
+void print_message(UINT8 *base, char message[], int x_pos, int y_pos) {
+	int i = 0;
+	if (x_pos >= 0 && x_pos < SCREEN_WIDTH && y_pos >= 0 && y_pos < SCREEN_HEIGHT) {
+		while (message[i] != '\0') {
+			plot_char((UINT8 *) base, x_pos, y_pos, message[i]);
+			x_pos += 8;
+			i++;
+		}
+	}
+}
