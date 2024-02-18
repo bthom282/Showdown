b03a049c6b05b32b4be0dc570676a57e7385409a
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-18 10:23:26 -0700

    Update model.c

--- model.c
+++ model.c
@@ -28,8 +28,9 @@ void move_bullets(struct Bullet *bullet,struct Bullet *active_bullets, int *bull
 		active_bullets[i].position.x += active_bullets[i].speed*active_bullets[i].x_dir;
   		active_bullets[i].position.y += active_bullets[i].speed*active_bullets[i].y_dir;
 
-		if (bullet->position.y<=0||bullet->position.y>=380||bullet->position.x<=256||bullet->position.x>=632) {
-			delete_bullet(bullet);
+		if (active_bullets[i].position.y<=0||active_bullets[i].position.y>=380||
+		    active_bullets[i].position.x<=256||active_bullets[i].position.x>=632) {
+			delete_bullet(active_bullets[i], i);
 		}
 	}
 }
