0235e217bf2564247d8ef3daf94773a4f428c1b8
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-18 20:12:50 -0700

    Update renderer.h

--- renderer.h
+++ renderer.h
@@ -5,9 +5,9 @@ void game_start();
 
 void render(const Model *model, UINT32 *base);
 
-void render_bullets(UINT8 *base, struct Bullet *active_bullets, const UINT8 *bitmap8, int bullets_fill);
-
 void render_cowboy(UINT32 *base, const *Cowboy);
+
+void render_bullets(UINT8 *base, struct Bullet *active_bullets, const UINT8 *bitmap8, int bullets_fill);
 
 void render_snakes(UINT32 *base, );
 
