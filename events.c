17e2546a4972e55255996594891aa71b2eb3bbec
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-19 14:08:37 -0700

    Update events.c

--- events.c
+++ events.c
@@ -169,10 +169,10 @@ Function Name: 	snake_death
 Details: 	This function is called every time a snake is killed by a bullet and is removed
 		from play.
 
-Sample Call:	snake_death(active_snakes[i]);
+Sample Call:	snake_death(active_snakes[i], i, snakes_fill, &cowboy1.scoreboard);
 *********************************************************************************************/
 
-void snake_death(struct Snake *snake, int index, int snakes_fill, struct Scoreboard scoreboard) {
+void snake_death(struct Snake *snake, int index, int snakes_fill, struct Scoreboard *scoreboard) {
     /*possible call to snake death animation here*/
     	if (index >= 0 && index < snakes_fill) {
        		active_snakes[index] = active_snakes[snakes_fill - 1];
