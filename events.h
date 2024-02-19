e2b017af36c45dd36e15ee7632fcb9f749f89f5a
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-19 11:30:42 -0700

    Update events.h

--- events.h
+++ events.h
@@ -4,7 +4,7 @@
 #define MAX_BULLETS 20
 #define MAX_SNAKES 30
 
-int checkCollision(BoundingBox box1, BoundingBox box2);
+int checkCollision(struct BoundingBox box1, struct BoundingBox box2);
 
 void shooting(UINT8 *base, struct Cowboy *cowboy, const UINT8 *bitmap8, 
 				struct Bullet *active_bullets, int *bullets_fill);
