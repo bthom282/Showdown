640f2d7f069787bb18aff9d68443dfe3debd2588
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-18 20:14:46 -0700

    Update renderer.h

--- renderer.h
+++ renderer.h
@@ -13,6 +13,8 @@ void render_snakes(UINT32 *base, );
 
 void render_side_panel(UINT32 *base, const UINT32 *bitmap);
 
+void render_level1(UINT32 *base, const UINT32 *bitmap);
+
 void render_score{const *Score, UINT8 *base);
 
 void render_lives{const *Lives, UINT16 *base);
