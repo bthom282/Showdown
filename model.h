1557ce487d6f24a5baad443ba4df949f3fc35477
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-19 11:28:50 -0700

    Update model.h

--- model.h
+++ model.h
@@ -90,7 +90,7 @@ struct Position target;
 int state; 		   /* state used for bitmap printing {0 - not moving/ moving down, 1 - moving left
 							      2 - moving right, 3 - moving up} */
 struct BoundingBox boundingBox;
-const UINT32 bitmap = snake_bitmap;
+const UINT32 bitmap;
 };
 
 struct Model
