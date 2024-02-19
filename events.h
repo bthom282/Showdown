7e097c9167d1ce490278fbb6e1a1673f59558533
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-19 00:00:47 -0700

    Update events.h

--- events.h
+++ events.h
@@ -9,7 +9,7 @@ int checkCollision(BoundingBox box1, BoundingBox box2);
 void shooting(UINT8 *base, struct Cowboy *cowboy, const UINT8 *bitmap8, 
 				struct Bullet *active_bullets, int *bullets_fill);
 
-void delete_bullet (struct Bullet *bullet, int index);
+void delete_bullet (struct Bullet *active_bullets[], int *bullet_fill, int index);
 
 unsigned int randInRange(unsigned long *seed, unsigned int min, unsigned int max);
 
