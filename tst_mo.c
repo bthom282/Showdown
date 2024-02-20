4acc912ee10c481fe94d4f3fc07c95c5178f6ebf
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-19 20:34:55 -0700

    Update tst_mo.c

--- tst_mo.c
+++ tst_mo.c
@@ -1,5 +1,42 @@
+#include <stdio.h>
 #include <osbind.h>
+#include "types.h"
 #include "model.h"
+#include "events.h"
+#include "renderer.h"
+#include "raster.h"
+#include "font.h"
+#include "bitmaps.h"
+
+struct Cowboy initializeCowboy() {
+    struct Cowboy cowboy;
+
+    cowboy.position.x = 424;
+    cowboy.position.y = 184;
+    cowboy.size.height = 16;
+    cowboy.size.width = 16;
+    cowboy.y_dir = 0;
+    cowboy.x_dir = 0;
+    cowboy.speed = 2;
+    cowboy.isMoving = FALSE;
+    cowboy.isFiring = FALSE;
+    cowboy.yFireDir = 0;
+    cowboy.xFireDir = 0;
+    cowboy.state = 0;
+	cowboy.scoreboard.score = 0;
+    cowboy.scoreboard.position.x = 80;
+    cowboy.scoreboard.position.y = 300;
+	cowboy.lives.lives_left = 3;
+    cowboy.lives.position.x = 80;
+    cowboy.lives.position.y = 320;
+	cowboy.boundingBox.top = cowboy.position.y;
+    cowboy.boundingBox.bottom = cowboy.position.y + cowboy.size.height;
+    cowboy.boundingBox.left = cowboy.position.x;
+    cowboy.boundingBox.right = cowboy.position.x + cowboy.size.width;
+	cowboy.bitmap = cowboy_bitmap;
+
+	return cowboy;
+}
 
 int main()
 {
@@ -25,7 +62,7 @@ int main()
 	print_message((UINT8 *)base, (UINT8 *)buffer, 32, 360);
 	
 	cowboy1.y_dir = 0;
-    	cowboy1.x_dir = 1;
+    cowboy1.x_dir = 1;
 	cowboy1.isMoving = TRUE;
 	
 	Cnecin(); 
