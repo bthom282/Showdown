1176ee152c3989e8f72b53f80176b857c19471d3
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-02-22 13:28:37 -0700

    Update model.c

--- model.c
+++ model.c
@@ -219,3 +219,68 @@ void respawn(struct Cowboy *cowboy) {
 	cowboy->position.y = 184;
 	/* invulnerability frames code here, asking Marc */
 }	
+
+/*******************************************************************************************
+Function Name: 	initializeCowboy()
+
+Details: 	This function initializes the Cowboy struct to it's default starting values.
+
+Sample Call:	cowboy1 = initializeCowboy();
+
+*********************************************************************************************/
+
+struct Cowboy initializeCowboy() {
+    struct Cowboy cowboy; 
+
+    cowboy.position.x = 424;
+    cowboy.position.y = 184;
+    cowboy.size.height = 32;
+    cowboy.size.width = 32;
+    cowboy.y_dir = 0;
+    cowboy.x_dir = 0;
+    cowboy.speed = 2;
+    cowboy.isMoving = FALSE;
+    cowboy.isFiring = FALSE;
+    cowboy.yFireDir = 0;
+    cowboy.xFireDir = 0;
+    cowboy.state = 0;
+	cowboy.scoreboard.score = 0;
+    cowboy.scoreboard.position.x = 80;
+    cowboy.scoreboard.position.y = 300;
+	cowboy.lives.lives_left = 3;
+    cowboy.lives.position.x = 80;
+    cowboy.lives.position.y = 320;
+	cowboy.boundingBox.top = cowboy.position.y;
+    cowboy.boundingBox.bottom = cowboy.position.y + cowboy.size.height;
+    cowboy.boundingBox.left = cowboy.position.x;
+    cowboy.boundingBox.right = cowboy.position.x + cowboy.size.width;
+
+	return cowboy;
+}
+
+/*******************************************************************************************
+Function Name: 	initializeSnake()
+
+Details: 	This function initializes the Snake struct to it's default spawning values.
+
+Sample Call:	cowboy1 = initializeCowboy();
+
+*********************************************************************************************/
+
+struct Snake initializeSnake() {
+	struct Snake snake;
+	
+	snake.position.x = 384;   
+	snake.position.y = 0; 	
+	snake.size.height = 32;
+	snake.size.width = 32;
+	snake.y_dir = 0;    		
+	snake.x_dir = 0;
+	snake.state = 0;
+	snake.boundingBox.top = snake.position.y;
+    snake.boundingBox.bottom = snake.position.y + snake.size.height;
+    snake.boundingBox.left = snake.position.x;
+    snake.boundingBox.right = snake.position.x + snake.size.width;
+	
+	return snake;
+}
