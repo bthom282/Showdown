f3114e5b71bf5ad2ba29123a2609d047e2a0c00e
bthom282 <113076978+bthom282@users.noreply.github.com>
2024-03-14 21:23:21 -0600

    Add files via upload

--- tst_ren.c
+++ tst_ren.c
@@ -1,5 +1,6 @@
 #include <osbind.h>
 #include <stdio.h>
+#include "model.h"
 #include "renderer.h"
 #include "events.h"
 
@@ -8,148 +9,137 @@ int main()
   /* series of snapshots, press any key to move cowboy. Score and lives changes as well as the cowboys position */
 	char *base = Physbase();
 	UINT32 seed = 1245;
-	int players = 1;
-	struct Bullet active_bullets[MAX_BULLETS];  /*array for active bullet structs*/
-	struct Snake active_snakes[MAX_SNAKES];    /*array for active snakes structs*/
-	int bullets_fill = 0;
-	int snakes_fill = 0;
 	int i;
-	char buffer[20];
-	int y_dir;
-	int x_dir;
-	int state;
-    int spawn_loc;
-	/*initializing the player1 cowboy*/
-	struct Cowboy cowboy1 = init_Cowboy();
+
+	struct Model model = init_Model();
+
+	/*struct Model testModelSnapshot = {
+
+	{},
+	{},
+	{},
+	{},
+	{},
+	{}
+
+	};*/
 		
 	/*render initial state*/
-	
-	render_side_panel((UINT16 *)base);
-	render_side_text((UINT8 *) base, players);
-	render_score((UINT8 *) base, &cowboy1.scoreboard);
-	render_lives((UINT16 *) base, cowboy1.lives, cowboy_lives);
-	render_level1((UINT32 *) base, (UINT32 *) cactus_bitmap);
-	cowboy1.state = 6;
-	render_cowboy((UINT32 *) base, cowboy1, (UINT32 *) cowboy_bitmap);
-	render_snakes((UINT32 *) base, active_snakes, snakes_fill, (UINT32 *) snake_bitmap);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
 
+	render((UINT32 *)base, &model); 
+
+	model.cowboy.state = 6;
+	
 	/*test rendering of different cowboy bitmaps*/
 	
 	Cnecin(); 
 	
-	clear_bitmap_32((UINT32 *) base, cowboy1.position.x, cowboy1.position.y, blank, BITMAP_32_HEIGHT);
-	cowboy1.state = 9;
+	clear_bitmap_32((UINT32 *) base, model.cowboy.position.x, model.cowboy.position.y, blank, BITMAP_32_HEIGHT);
+	model.cowboy.state = 9;
 	
 	Cnecin(); 
 
 	fill_screen((UINT32 *) base, 0);
+	model.cowboy.state = 4;
+	increase_score(&model.cowboy.scoreboard, 100);	
 
-	increase_score(&cowboy1.scoreboard, 100);	
-	render_side_panel((UINT16 *)base);
-	render_side_text((UINT8 *) base, players);
-	render_score((UINT8 *) base, &cowboy1.scoreboard);
-	render_lives((UINT16 *) base, cowboy1.lives, cowboy_lives);
-	render_level1((UINT32 *) base, (UINT32 *) cactus_bitmap);
-	cowboy1.state = 4;
-	render_cowboy((UINT32 *) base, cowboy1, (UINT32 *) cowboy_bitmap);
-	render_snakes((UINT32 *) base, active_snakes, snakes_fill, (UINT32 *) snake_bitmap);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	
-	spawn_snakes(active_snakes, &snakes_fill, &seed);
-	spawn_snakes(active_snakes, &snakes_fill, &seed);
-	spawn_snakes(active_snakes, &snakes_fill, &seed);
-	spawn_snakes(active_snakes, &snakes_fill, &seed);
-
-	render_snakes((UINT32 *) base, active_snakes, snakes_fill, (UINT32 *) snake_bitmap);
+	spawn_snakes(model.active_snakes, &model.snakes_fill, &seed);
+	spawn_snakes(model.active_snakes, &model.snakes_fill, &seed);
+	spawn_snakes(model.active_snakes, &model.snakes_fill, &seed);
+	spawn_snakes(model.active_snakes, &model.snakes_fill, &seed);
 
 	/*show snake movement*/
 	
-	cowboy1.isMoving = TRUE;
-	cowboy1.y_dir = -1;
-    cowboy1.x_dir = 1;
+	model.cowboy.isMoving = TRUE;
+	model.cowboy.y_dir = -1;
+    model.cowboy.x_dir = 1;
 	
 	for (i = 0; i < 40; i++)  {
-		move_snakes(active_snakes, snakes_fill, cowboy1);
-		move_cowboy(&cowboy1);
+		move_snakes(model.active_snakes, model.snakes_fill, model.cowboy);
+		move_cowboy(&model.cowboy);
 	}
+
+	render((UINT32 *)base, &model);
 	
 	Cnecin(); 
 
 	fill_screen((UINT32 *) base, 0);
 	
-	cowboy1.lives.lives_left = 2;
+	model.cowboy.lives.lives_left = 2;
 	
-	render_side_panel((UINT16 *)base);
-	render_side_text((UINT8 *) base, players);
-	render_score((UINT8 *) base, &cowboy1.scoreboard);
-	render_lives((UINT16 *) base, cowboy1.lives, cowboy_lives);
-	render_level1((UINT32 *) base, (UINT32 *) cactus_bitmap);
-	cowboy1.state = 11;
-	render_cowboy((UINT32 *) base, cowboy1, (UINT32 *) cowboy_bitmap);
-	render_snakes((UINT32 *) base, active_snakes, snakes_fill, (UINT32 *) snake_bitmap);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
+	model.cowboy.state = 11;
 	
 	/*shows cowboy firing in all directions*/
 	
-	cowboy1.isFiring = TRUE;
-	cowboy1.yFireDir = -1;
-    cowboy1.xFireDir = -1;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = 0;
-    cowboy1.xFireDir = -1;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = 1;
-    cowboy1.xFireDir = -1;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = 1;
-    cowboy1.xFireDir = 0;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = 1;
-    cowboy1.xFireDir = 1;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = 0;
-    cowboy1.xFireDir = 1;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = -1;
-    cowboy1.xFireDir = 1;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
-	cowboy1.yFireDir = -1;
-    cowboy1.xFireDir = 0;
-	shooting(&cowboy1, active_bullets, &bullets_fill);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
+	model.cowboy.isFiring = TRUE;
+	model.cowboy.yFireDir = -1;
+    model.cowboy.xFireDir = -1;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+
+	model.cowboy.yFireDir = 0;
+    model.cowboy.xFireDir = -1;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+
+	model.cowboy.yFireDir = 1;
+    model.cowboy.xFireDir = -1;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+
+	model.cowboy.yFireDir = -1;
+    model.cowboy.xFireDir = 0;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+
+	model.cowboy.yFireDir = 1;
+    model.cowboy.xFireDir = 1;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+	
+	model.cowboy.yFireDir = 0;
+    model.cowboy.xFireDir = 1;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+
+	model.cowboy.yFireDir = -1;
+    model.cowboy.xFireDir = 1;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	/*render((UINT32 *)base, &model);*/
+
+	model.cowboy.yFireDir = -1;
+    model.cowboy.xFireDir = 0;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
+	
+	model.cowboy.yFireDir = 1;
+    model.cowboy.xFireDir = 0;
+	shooting(&model.cowboy, model.active_bullets, &model.bullets_fill);
 	
 	
+	render((UINT32 *)base, &model);
+
 	/*to show bullet movement*/
-	
-	move_bullets(active_bullets, &bullets_fill, active_snakes, &snakes_fill, cowboy1);
-	move_bullets(active_bullets, &bullets_fill, active_snakes, &snakes_fill, cowboy1);
-	move_bullets(active_bullets, &bullets_fill, active_snakes, &snakes_fill, cowboy1);
-	move_bullets(active_bullets, &bullets_fill, active_snakes, &snakes_fill, cowboy1);
-	move_bullets(active_bullets, &bullets_fill, active_snakes, &snakes_fill, cowboy1);
+	move_bullets(model.active_bullets, &model.bullets_fill, model.active_snakes,
+				 &model.snakes_fill, model.cowboy);
+	move_bullets(model.active_bullets, &model.bullets_fill, model.active_snakes,
+				 &model.snakes_fill, model.cowboy);
+	move_bullets(model.active_bullets, &model.bullets_fill, model.active_snakes,
+				 &model.snakes_fill, model.cowboy);
+	move_bullets(model.active_bullets, &model.bullets_fill, model.active_snakes,
+				 &model.snakes_fill, model.cowboy);
+	move_bullets(model.active_bullets, &model.bullets_fill, model.active_snakes,
+				 &model.snakes_fill, model.cowboy);
 	
 	Cnecin(); 
 	
-	cowboy1.lives.lives_left = 1;
-	wave_bonus(&cowboy1.scoreboard);
+	model.cowboy.lives.lives_left = 1;
+	wave_bonus(&model.cowboy.scoreboard);
 	fill_screen((UINT32 *) base, 0);
 	
-	render_side_panel((UINT16 *)base);
-	render_side_text((UINT8 *) base, players);
-	render_score((UINT8 *) base, &cowboy1.scoreboard);
-	render_lives((UINT16 *) base, cowboy1.lives, cowboy_lives);
-	render_level1((UINT32 *) base, (UINT32 *) cactus_bitmap);
-	cowboy1.state = 1;
-	render_cowboy((UINT32 *) base, cowboy1, (UINT32 *) cowboy_bitmap);
-	render_snakes((UINT32 *) base, active_snakes, snakes_fill, (UINT32 *) snake_bitmap);
-	render_bullets((UINT8 *) base, active_bullets, (UINT8 *) bullet_bitmap, bullets_fill);
+	model.cowboy.state = 1;
+	
+	render((UINT32 *)base, &model);
+
 
 #ifdef FOO
 	char *base = Physbase();
@@ -188,7 +178,7 @@ int main()
 	cowboy1.yFireDir = 1;
 	cowboy1.xFireDir = -1;
 	cowboy1.isFiring = TRUE;
-	shooting(&cowboy1, (UINT8 *)bullet_bitmap, active_bullets, &bullets_fill);
+	shooting((UINT8 *)base, &cowboy1, (UINT8 *)bullet_bitmap, active_bullets, &bullets_fill);
 
 	render_side_panel((UINT16 *)base);
 	render_side_text((UINT8 *) base, players);
