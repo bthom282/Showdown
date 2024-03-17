4bdfbe77ae39ff194fc41d183d7b72680613f8c8
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-03-17 12:06:36 -0600

    Add files via upload

---
+++ showdown.h
@@ -0,0 +1,20 @@
+#ifndef SHOWDOWN_H
+#define SHOWDOWN_H
+
+#include <osbind.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "model.h"
+#include "events.h"
+#include "renderer.h"
+#include "types.h"
+#include "raster.h"
+
+
+
+UINT32 getTime();
+
+UINT32* getBuffer();
+
+#endif
