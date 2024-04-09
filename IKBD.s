92f26c25fbff148067271c31ff9f0a3f56b8d730
Adam-Cole <113710347+Adam-Cole@users.noreply.github.com>
2024-04-08 22:50:02 -0600

    Add files via upload

--- IKBD.s
+++ IKBD.s
@@ -1,8 +1,10 @@
 		xdef	_ikbd_isr
-		xref	_ikbd_isr_c
+		xref	_ikbdisrC
 		
 _ikbd_isr:
+			link	a6,#0
 			movem.l	d0-d2/a0-a2,-(sp)
-			jsr	_ikbd_isr_c
+			jsr	_ikbdisrC
 			movem.l	(sp)+,d0-d2/a0-a2
+			unlk	a6
 			rte
--- vbl.s
+++ vbl.s
@@ -2,7 +2,9 @@
 	xref	_vblisrC
 	
 _vbl_isr:
+			link	a6,#0
 			movem.l	d0-d2/a0-a2,-(sp)
 			jsr		_vblisrC
 			movem.l	(sp)+,d0-d2/a0-a2
+			unlk	a6
 			rte
